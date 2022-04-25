6144a246768bf3ccec05cb7acad39c61f504ec05
AndreyOhotnikov <ququruq@gmail.com>
2022-04-25 08:46:44 +0300

    pre filesUpload

--- server/src/track/tarck.controller.ts
+++ server/src/track/tarck.controller.ts
@@ -1,16 +1,46 @@
-import { Controller, Get } from "@nestjs/common";
-
+import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { CreateTrackDto } from "./dto/create-track.dto";
+import { TrackService } from "./track.service";
+import * as mongoose from 'mongoose'
+import { Track } from "./schems/track.schema";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
 
 @Controller('/tracks')
 
 export class TrackController {
-  create() {}
-  @Get()
-  getAll() {
-    return 'get all'
+  constructor(private trackService: TrackService) {
+
   }
 
-  getOne() {}
+  @Post()
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'picture', maxCount: 1 },
+    { name: 'audio', maxCount: 1 },
+  ]))
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    console.log(files)
+    const {picture, audio} = files
+    return this.trackService.create(dto, picture[0], audio[0])
+  }
 
-  delete() {}
+  @Get()
+  getAll() {
+    return this.trackService.getAll()
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: mongoose.Schema.Types.ObjectId) {
+    return this.trackService.getOne(id)
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: mongoose.Schema.Types.ObjectId) {
+    return this.trackService.delete(id);
+  }
+
+  @Post('/comment')
+  addComment(@Body() dto: CreateCommentDto) {
+    return this.trackService.addComment(dto);
+  }
 }
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -2,19 +2,45 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Track, TrackDocument } from "./schems/track.schema";
+import { Comment, CommentDocument } from './schems/comment.schema';
+import { CreateTrackDto } from './dto/create-track.dto';
+import mongoose from 'mongoose';
+import { CreateCommentDto } from './dto/create-comment.dto';
+
 
 
 @Injectable()
 
 export class TrackService{
 
-  constructor(@InjectModel(Track.name) private catModel: Model<TrackDocument>) {}
+  constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+              @InjectModel(Comment.name) private commentModel: Model<CommentDocument>) {}
 
-  async create() {}
+  async create(dto: CreateTrackDto, pictures: string, audio: string): Promise<Track> {
+    const track = await this.trackModel.create({...dto, listens: 0})
+    return track;
+  }
 
-  async getAll() {}
+  async getAll(): Promise<Track[]> {
+    const tracks = await this.trackModel.find();
+    return tracks;
+  }
 
-  async getOne() {}
+  async getOne(id: mongoose.Schema.Types.ObjectId): Promise<Track> {
+    const track = await (await this.trackModel.findById(id)).populate('comments');
+    return track;
+  }
 
-  async delete() {}
+  async delete(id: mongoose.Schema.Types.ObjectId): Promise<mongoose.Schema.Types.ObjectId> {
+    const track = await this.trackModel.findByIdAndDelete(id);
+    return track._id
+  }
+
+  async addComment(dto: CreateCommentDto): Promise<Comment>  {
+    const track = await this.trackModel.findById(dto.trackId);
+    const comment = await this.commentModel.create({...dto});
+    track.comments.push(comment._id);
+    await track.save();
+    return comment;
+  }
 }
