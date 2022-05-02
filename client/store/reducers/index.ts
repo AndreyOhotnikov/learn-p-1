f9d9d471c53131c0307c03c9b1a70a298653426e
AndreyOhotnikov <ququruq@gmail.com>
2022-05-02 08:32:52 +0300

    reducer

--- client/store/reducers/index.ts
+++ client/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
+import { playerReducer } from './playerReducer'
 
-const rootReducer = combineReducers({
-
+export const rootReducer = combineReducers({
+  player: playerReducer,
 })
 
 
+export type RootState = ReturnType<(typeof rootReducer)>
--- client/types/player.ts
+++ client/types/player.ts
@@ -11,7 +11,44 @@ export interface PlayerState {
 export enum PlayerActionTypes {
   PLAY = 'PLAY',
   PAUSE='PAUSE',
-  SET_DURATION='SET_DURATION',
-  SET_CTIVE,
-  
+  SET_DURATION = 'SET_DURATION',
+  SET_ACTIVE = 'SET_ACTIVE',
+  SET_CURRENT_TIME = 'SET_CURRENT_TIME',
+  SET_VOLUME = 'SET_VOLUME'
 }
+
+interface PlayAction {
+  type: PlayerActionTypes.PLAY
+}
+
+interface PauseAction {
+  type: PlayerActionTypes.PAUSE
+}
+
+interface SetActiveAction {
+  type: PlayerActionTypes.SET_ACTIVE;
+  payload: ITrack;
+}
+
+interface SetDurationAction {
+  type: PlayerActionTypes.SET_DURATION;
+  payload: number;
+}
+
+interface SetCurrentTimeAction {
+  type: PlayerActionTypes.SET_CURRENT_TIME;
+  payload: number;
+}
+
+interface SetVolumeAction {
+  type: PlayerActionTypes.SET_VOLUME;
+  payload: number;
+}
+
+export type PlayerAction = 
+  PlayAction
+  | PauseAction
+  | SetActiveAction
+  | SetDurationAction
+  | SetCurrentTimeAction
+  | SetVolumeAction
