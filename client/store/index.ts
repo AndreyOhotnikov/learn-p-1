8c5331cae2388fd5a9706ea0e73d13ca51835c34
AndreyOhotnikov <ququruq@gmail.com>
2022-05-02 08:35:02 +0300

    reducer2

--- client/store/index.ts
+++ client/store/index.ts
@@ -1,8 +1,8 @@
 import { Context, createWrapper, MakeStore } from "next-redux-wrapper";
 import { createStore } from "redux";
-import { rootReducer, RootState } from "./reducers";
+import { reducer, RootState } from "./reducers";
 
 
-const makeStore: MakeStore<RootState> = (context: Context) => createStore(rootReducer);
+const makeStore: MakeStore<RootState> = (context: Context) => createStore(reducer);
 
 export const wrapper = createWrapper<RootState>(makeStore, {debug: true})
